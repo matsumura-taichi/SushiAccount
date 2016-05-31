0fe8b8aea97b7827cde5b16bbd0aafb9a49d77a8
matsumura-taichi <matsumura.taichi@nichiwa-system.co.jp>
2016-05-31 19:21:34 +0900

    1~4週の場合割引なし、5~9週の場合1割引、10週以上の場合2割引を確認、緑になった

--- src/main/java/jp/co/nichiwa_system/Plate.java
+++ src/main/java/jp/co/nichiwa_system/Plate.java
@@ -7,33 +7,42 @@ public class Plate {
 	private static final String TYPE_YELLOW = "YELLOW";
 
 	private String type;
+	private int lap;
 
-	private Plate(String type) {
+	private Plate(String type, int lap) {
 		this.type = type;
+		this.lap = lap;
 	}
 
-	public int getPrice() {
-		switch (type) {
-		case TYPE_RED:
-			return 100;
-		case TYPE_BLUE:
-			return 300;
-		case TYPE_YELLOW:
-			return 450;
-		default:
-			return 0;
+	public double getPrice() {
+		double price = 0;
+
+		if (type.equals(TYPE_RED)) {
+			price = 100;
+		} else if (type.equals(TYPE_BLUE)) {
+			price = 300;
+		} else if (type.equals(TYPE_YELLOW)) {
+			price = 450;
 		}
+
+		if(lap >= 5 && lap <= 9) {
+			price = price * 0.9;
+		} else if(lap >= 10) {
+			price = price * 0.8;
+		}
+
+		return price;
 	}
 
 	public static Plate createRedPlate(int lap) {
-		return new Plate(TYPE_RED);
+		return new Plate(TYPE_RED, lap);
 	}
 
 	public static Plate createBluePlate(int lap) {
-		return new Plate(TYPE_BLUE);
+		return new Plate(TYPE_BLUE, lap);
 	}
 
 	public static Plate createYellowPlate(int lap) {
-		return new Plate(TYPE_YELLOW);
+		return new Plate(TYPE_YELLOW, lap);
 	}
 }
